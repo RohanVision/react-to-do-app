306c2ed77fa0db825d3c98ff45f2ec432dab314f
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-11-11 17:14:00 -0500

    Made Reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaPlus, FaPencilAlt, FaTrash } from "react-icons/fa";
+import { FaPlus, FaPencilAlt } from "react-icons/fa";
 import {
 	addDoc,
 	collection,
@@ -10,6 +10,7 @@ import {
 } from "firebase/firestore";
 import { db } from "./firebase";
 import "./App.css";
+import TodoLists from "./components/todoLists";
 
 function App() {
 	const [todos, setTodos] = useState([]); // to updated todos
@@ -105,32 +106,11 @@ function App() {
 			</div>
 
 			{todos.length > 0 && (
-				<div className="bg-gray-100 p-6 rounded shadow-md w-full max-w-lg lg:w-1/4">
-					<ul>
-						{todos.map((todo, index) => (
-							<li
-								key={index}
-								className="flex items-start justify-between bg-white p-3 rounded shadow-md mb-3"
-							>
-								<span className="text-lg">{todo.todo}</span>
-								<div>
-									<button
-										onClick={() => setEdit(index)}
-										className="mr-2 p-2 bg-gradient-to-r from-gray-400 to-gray-600 text-white rounderd hover:from-gray-500 hover:to-gray-700"
-									>
-										<FaPencilAlt />
-									</button>
-									<button
-										onClick={() => removeTodo(todo.id)}
-										className="mr-2 p-2 bg-gradient-to-r from-red-400 to-red-600 text-white rounderd hover:from-red-500 hover:to-red-700"
-									>
-										<FaTrash />
-									</button>
-								</div>
-							</li>
-						))}
-					</ul>
-				</div>
+				<TodoLists
+					todos={todos}
+					setEdit={setEdit}
+					removeTodo={removeTodo}
+				/>
 			)}
 		</div>
 	);
