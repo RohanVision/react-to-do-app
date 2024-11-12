532e4710e852cac014fbf836e5c41ac44ce81faf
Rohan Amberkar <amberkar.rohan17@gmail.com>
2024-11-11 21:34:57 -0500

    dotEnv updated for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC1dvuBKzmQ9rELpvZaQDgf3RnNZvl5FPM",
-    authDomain: "todo-app-9e586.firebaseapp.com",
-    projectId: "todo-app-9e586",
-    storageBucket: "todo-app-9e586.firebasestorage.app",
-    messagingSenderId: "412985911859",
-    appId: "1:412985911859:web:c8356f2c9131283ff75146"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
